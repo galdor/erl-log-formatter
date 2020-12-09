b7c7ba1ce3225830c0a86d6d19d097494eed9abc
Nicolas Martyanoff <khaelin@gmail.com>
2020-12-09 10:32:41 +0100

    fix indentation and padding for event lines
    This is getting ridiculous.

--- src/log_formatter_text.erl
+++ src/log_formatter_text.erl
@@ -14,7 +14,7 @@
 
 -module(log_formatter_text).
 
--export([format/4]).
+-export([format/4, last_line/1]).
 
 -spec format(unicode:chardata(), logger:level(), logger:metadata(),
              log_formatter:config()) ->
@@ -40,20 +40,23 @@ format(String0, Level, Metadata, Config) ->
                          [TimeString,
                           9, log_formatter:format_level(Level),
                           24 + DomainLengthDiff, DomainString]),
-  {String, StringLengthDiff} =
+  {String, ExtraPrefixLength, StringLengthDiff} =
     case maps:find(event, Metadata) of
       {ok, Event} ->
         EventString = log_formatter:format_event(Event),
         EventPrefix0 = [$[, EventString, $], $\s],
         {EventPrefix, EventPrefixLengthDiff} =
           maybe_colorize(EventPrefix0, yellow, Config),
-        {[EventPrefix, String0], EventPrefixLengthDiff};
+        {[EventPrefix, String0], string:length(EventPrefix0),
+         EventPrefixLengthDiff};
       error ->
-        {String0, 0}
+        {String0, 0, 0}
     end,
   TrimmedString = string:trim(String, trailing, " \n\t"),
-  PaddedString = pad_multiline_string(TrimmedString, 80 + StringLengthDiff),
-  Indent = iolist_size(Prefix) - StringLengthDiff,
+  Padding = 80 + StringLengthDiff,
+  PaddedString = pad_multiline_string(TrimmedString, Padding,
+                                      ExtraPrefixLength+StringLengthDiff),
+  Indent = iolist_size(Prefix) + ExtraPrefixLength - StringLengthDiff,
   IndentedString = indent_multiline_string(PaddedString, Indent),
   IgnoredMetadata = [domain, time, event, % duplicate
                      error_logger, logger_formatter, report_cb, % useless
@@ -74,34 +77,27 @@ indent_multiline_string(String, N) ->
   Padding = lists:duplicate(N, $\s),
   string:replace(String, "\n", [$\n, Padding], all).
 
--spec pad_multiline_string(unicode:chardata(), non_neg_integer()) ->
+-spec pad_multiline_string(unicode:chardata(), non_neg_integer(),
+                           non_neg_integer()) ->
         unicode:chardata().
-pad_multiline_string(String, N) ->
+pad_multiline_string(String, Padding, FirstLinePrefixLength) ->
   Bin = unicode:characters_to_binary(String),
-  case string:length(last_line(Bin)) of
-    Len when Len < N ->
-      Padding = lists:duplicate(N-Len, $\s),
-      [Bin, Padding];
+  {LastLine, NbLines} = last_line(Bin),
+  case string:length(LastLine) of
+    Len when Len < Padding, NbLines > 1 ->
+      [Bin, lists:duplicate(Padding-Len-FirstLinePrefixLength, $\s)];
+    Len when Len < Padding ->
+      [Bin, lists:duplicate(Padding-Len, $\s)];
     _ ->
       Bin
   end.
 
--spec last_line(binary()) -> binary().
+-spec last_line(binary()) -> {binary(), NbLines :: non_neg_integer()}.
 last_line(<<>>) ->
-  <<>>;
+  {<<>>, 0};
 last_line(Bin) ->
-  case binary:last(Bin) of
-    $\n ->
-      last_line(binary:part(Bin, 0, byte_size(Bin)-1));
-    _ ->
-      case binary:matches(Bin, <<"\n">>) of
-        [] ->
-          Bin;
-        Matches ->
-          {Pos, _} = lists:last(Matches),
-          binary:part(Bin, Pos+1, byte_size(Bin)-Pos-1)
-      end
-  end.
+  Lines = binary:split(Bin, <<"\n">>, [global, trim]),
+  {lists:last(Lines), length(Lines)}.
 
 -spec format_time(SystemTime :: integer()) -> unicode:chardata().
 format_time(SystemTime) ->
